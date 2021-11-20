5798fe8852dea4a043fdfc2639e42400ea657ba4
Keshav Gambhir <keshav19249@iiitd.ac.in>
2021-11-20 20:29:22 +0530

    question 2 completed

--- question2.R
+++ question2.R
@@ -1,6 +1,8 @@
 rm(list = ls())
-set.seed(12345)
-p <- (matrix(c( 1/3, 2/3,   0,   0,  0,   0,   0,   0,
+
+
+#Question-2 Part A
+p <- t(matrix(c( 1/3, 2/3,   0,   0,  0,   0,   0,   0,
                 1/3, 1/3, 1/3,   0,  0,   0,   0,   0,
                 0, 1/3, 1/3, 1/3,  0,   0,   0,   0,
                 0,   0, 1/3, 1/3, 1/3,  0,   0,   0,
@@ -28,3 +30,27 @@ for(i in 1:50) {
 matplot(finalStateMatrix, type='l', lty=1, col=1:5, ylim=c(0,7), ylab='state', xlab='time')
 abline(h=0, lty=3)
 abline(h=7, lty=3)
+
+#Question-2 Part B
+transitionMatrix = diag(8)
+pi <- matrix(c(1,0,0,0,0,0,0,0),nrow=1,ncol=8)
+
+i <- 1
+while(i <= 50) {
+  pi <- pi %*% p
+  transitionMatrix <- transitionMatrix %*% p
+  
+  if(i == 10) {
+    print("P == 10")
+    print(transitionMatrix)
+  }
+  if(i == 20) {
+    print("P == 20")
+    print(transitionMatrix)
+  }
+  if(i == 50) {
+    print("P == 50")
+    print(transitionMatrix)
+  }
+  i <- i+1
+}
